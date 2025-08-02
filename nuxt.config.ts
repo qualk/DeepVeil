6e9b5a6c64ee093986250a8b2de5dd992a135bc9
qualk <qualk@qualk.xyz>
2025-08-02 14:29:56 +1000

    biolerplate dosne

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
     '@nuxt/ui',
-    '@nuxthub/core'
+    '@nuxthub/core',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/seo',
+    'nuxt-booster',
   ],
 
   hub: {
     // Hub configuration options
-  }
-})
+  },
+
+  image: {
+    domains: ['img.youtube.com', 'i.vimeocdn.com'],
+    alias: {
+      youtube: 'https://img.youtube.com',
+      vimeo: 'https://i.vimeocdn.com',
+    },
+  },
+});
